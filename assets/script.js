e322c996bf0a21481c113acda08ad7c4fcfd1d7b
Ana Ea <aabad0464@gmail.com>
2021-05-05 00:57:23 -0600

    added function passwordSize as length

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,24 @@ var upperConfirm;
 var numberConfirm;
 var symbolConfirm;
 var totalChar;
+
+// //User Choices
+// Lenght of Password
+
+function passwordSize(){
+  usersLength = prompt(`Choose a length of at least 8 characters and no more than 128 characters`);
+
+    if (usersLength<8){
+      alert(`Password lenght MUST be at least 8 characters`);
+      passwordSize();
+
+    }else if (usersLength>128){
+      alert(`Password length MUST be no more than 128 characters`);
+      passwordSize();
+
+    }else if (isNaN(usersLength)){
+      alert(`Password length MUST be between 8-128 characters`);
+      passwordSize();
+    }
+    return usersLength;
+}
